ebcda8da0adfa0287e7ec81aa250ed80c76a20aa
Jeremy Watt <jermwatt@gmail.com>
2025-01-24 14:31:22 -0700

    lint and test update

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,4 @@ import os
 
 cwd = os.getcwd()
 CONTAINER_NAME = "bleep_that_sht"
-STREAMLIT_UPLOAD_FILE = "bleep_that_sht/streamlit_app_video_upload.py"
-STREAMLIT_DOWNLOAD_FILE = "bleep_that_sht/streamlit_app_url_download.py"
+STREAMLIT_MERGED_FILE = "bleep_that_sht/app.py"
