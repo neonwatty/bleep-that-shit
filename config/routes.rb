0e70bdb3a5efe45c24268944ddb9244e5b4224a8
neonwatty <jermwatt@gmail.com>
2025-06-29 17:03:43 -0700

    new home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
-  root "sessions#new"
+  root "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
